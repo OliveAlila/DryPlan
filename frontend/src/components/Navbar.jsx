0cfd262a19acb1623c09583a78bc383a3d6e1eff
Natasha <natashahinga58@gmail.com>
2025-10-05 02:09:15 +0300

    add dashboard

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,24 @@
+// components/Navbar.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // optional CSS file
+import styles from "./Navbar.module.css"; // Changed to module
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className={styles.navbar}>
+      {" "}
+      {/* Use module class */}
       <h1>🌦️ DryPlan</h1>
       <ul>
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/details">Event Details</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/details">Event Details</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
